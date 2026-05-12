67b196ce305009bb19e2e3a62b5a99fa28e72984
jzonthemtn <jeff.zemerick@mtnfog.com>
2026-05-11 21:31:22 -0400

    Fixing test.
    Signed-off-by: jzonthemtn <jeff.zemerick@mtnfog.com>

--- src/test/java/org/opensearch/ubi/UbIParametersTests.java
+++ src/test/java/org/opensearch/ubi/UbIParametersTests.java
@@ -9,24 +9,18 @@
 package org.opensearch.ubi;
 
 import org.opensearch.action.search.SearchRequest;
+import org.opensearch.common.xcontent.XContentFactory;
 import org.opensearch.core.common.io.stream.StreamOutput;
-import org.opensearch.core.xcontent.XContent;
 import org.opensearch.core.xcontent.XContentBuilder;
-import org.opensearch.core.xcontent.XContentGenerator;
 import org.opensearch.search.builder.SearchSourceBuilder;
 import org.opensearch.test.OpenSearchTestCase;
 import org.opensearch.ubi.ext.UbiParameters;
 import org.opensearch.ubi.ext.UbiParametersExtBuilder;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class UbIParametersTests extends OpenSearchTestCase {
 
@@ -103,22 +97,18 @@ public class UbIParametersTests extends OpenSearchTestCase {
 
     public void testToXContent() throws IOException {
         final UbiParameters params = new UbiParameters("query_id", "user_query", "client_id", "app", "object_id", Collections.emptyMap());
-        XContent xc = mock(XContent.class);
-        OutputStream os = mock(OutputStream.class);
-        XContentGenerator generator = mock(XContentGenerator.class);
-        when(xc.createGenerator(any(), any(), any())).thenReturn(generator);
-        XContentBuilder builder = new XContentBuilder(xc, os);
+        XContentBuilder builder = XContentFactory.jsonBuilder();
+        builder.startObject();
         assertNotNull(params.toXContent(builder, null));
+        builder.endObject();
     }
 
     public void testToXContentAllOptionalParameters() throws IOException {
         final UbiParameters params = new UbiParameters("query_id", "user_query", "client_id", "app", "object_id", Collections.emptyMap());
-        XContent xc = mock(XContent.class);
-        OutputStream os = mock(OutputStream.class);
-        XContentGenerator generator = mock(XContentGenerator.class);
-        when(xc.createGenerator(any(), any(), any())).thenReturn(generator);
-        XContentBuilder builder = new XContentBuilder(xc, os);
+        XContentBuilder builder = XContentFactory.jsonBuilder();
+        builder.startObject();
         assertNotNull(params.toXContent(builder, null));
+        builder.endObject();
     }
 
 }
